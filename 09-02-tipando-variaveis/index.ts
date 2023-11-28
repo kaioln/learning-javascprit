44beacd2022cac354131274cf6dc349565d19037
KaioLN <Kaio.LeonardoSS@gmail.com>
2023-11-28 16:08:58 -0300

    Aprendendo a tipar no Typescript

---
+++ 09-02-tipando-variaveis/index.ts
@@ -0,0 +1,4 @@
+const personName:string = 'Daniel'
+const personAge:number = 23
+const personAlive:boolean = true
+const personContacts:(string|number)[] = ['Kaio.LeonardoSS@gmail.com', 21998767483]
