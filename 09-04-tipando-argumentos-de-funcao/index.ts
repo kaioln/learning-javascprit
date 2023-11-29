d150dda46ba2ad6b85bfc6e0af701b49363c00f0
KaioLN <Kaio.LeonardoSS@gmail.com>
2023-11-29 14:25:27 -0300

    Aprendendo a tipar argumentos de funções.

---
+++ 09-04-tipando-argumentos-de-funcao/index.ts
@@ -0,0 +1,15 @@
+function greet(name: string | number) {
+    console.log(
+        `hello, ${name.toUpperCase()}!` //Typescript não aceita uppercase em number.
+    )
+}
+
+greet('Daniel')
+
+function greety(name: string, age?:number) {
+    console.log(
+        `hello, ${name.toUpperCase()}!`
+    )
+}
+
+greety('Daniel', 12) //posso passar sem número também por conta do '?'
