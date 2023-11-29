a3968373c7973d29b9b44f987a8efb52cec83195
KaioLN <Kaio.LeonardoSS@gmail.com>
2023-11-29 15:49:04 -0300

    ajuste e correção tipando objetos

--- 09-05-tipando-objetos/index.js
+++ 09-05-tipando-objetos/index.js
@@ -1,1 +1,14 @@
 "use strict";
+const person = {
+    age: 15,
+    name: 'Lucas',
+    isAlive: true,
+    contacts: [
+        'kaio.leonardoss@gmail.com'
+    ]
+};
+const person2 = {
+    age: 18,
+    name: 'Pedro',
+    isAlive: true,
+};
